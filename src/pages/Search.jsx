4d22f744ee7ca1a5bbb33e1d29237330112931d4
Arthur Felipe <arthur_avf.br@hotmail.com>
2023-05-01 19:36:53 -0300

    finalização do projeto

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import "../pages/MoviesGrid.css";
+
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setmovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getTopRatedMovies = async(url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setmovies(data.results);
+  };
+
+  useEffect(()=>{
+
+    const searchWidthQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+    getTopRatedMovies(searchWidthQueryURL);
+    
+  }, [query]);
+
     return (
-      <div>Search</div>
-    )
-  }
+    <div className="conatiner">
+    <h2 className="title">
+      Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length === 0 && <p>Carregando...</p>}
+        {movies.length > 0 && 
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+    );
+  };
   
   export default Search
